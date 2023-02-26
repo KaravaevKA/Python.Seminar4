963d7e6877c3a3b62044b63d1fb5a978f2f4f8aa
Kirill Karavaev <coffee.2016@yandex.ru>
2023-02-26 15:16:03 +0300

    Решение задачи 24

--- main.py
+++ main.py
@@ -111,3 +111,14 @@
 # u = a.union(b)
 
 # print(sorted(u))
+
+# Задача 24
+# n = int(input())
+# arr = list()
+# for i in range(n):
+#     x = int(input())
+#     arr.append(x)
+# arr_count = list()
+# for i in range(len(arr)-1):
+#     arr_count.append(arr[-2]+arr[-1]+arr[0])
+#     print(max(arr_count))
